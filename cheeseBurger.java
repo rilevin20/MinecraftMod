26e87c19ef010e05a7ffd4bdda76740e2586608e
rilevin20 <43276013+rilevin20@users.noreply.github.com>
2019-05-24 09:13:11 -0700

    Add files via upload

---
+++ cheeseBurger.java
@@ -0,0 +1,16 @@
+package tealsmc.mods.items;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+public class cheeseBurger extends Item {
+
+    public cheeseBurger() {
+    	setMaxStackSize(1);
+    	setCreativeTab(CreativeTabs.tabDecorations);
+    }
+    @Override
+    
+}
